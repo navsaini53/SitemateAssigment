b1b434735a591db4ce932dcc3793596d760d9521
naveen <navsaini54@gmail.com>
2024-06-18 20:52:25 +1000

    added some styles

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, FlatList, ActivityIndicator, TextInput, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, FlatList, StatusBar, ActivityIndicator, TextInput, Text, View, TouchableOpacity } from 'react-native';
 
 import { sendOtpAction } from './request';
 import FlatItem from "./flatItem";
 
-
+const primaryColor = '#211f4a'
 
 export default function App() {
 
@@ -20,6 +20,7 @@ export default function App() {
     setResults([])
     setTotal(0)
     sendOtpAction({ searchTxt }).then(({ articles, totalResults, status }) => {
+      console.log(status)
       if (status == 'ok') {
         setResults(articles)
         setTotal(totalResults)
@@ -30,48 +31,60 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
+      <StatusBar
+        animated={true}
+        backgroundColor={primaryColor}
 
-      <StatusBar style="auto" />
-      <View style={styles.inputCtrl}>
-        <View style={{ flex: 1 }}>
-          <TextInput style={styles.input}
-            value={searchTxt}
-            onChangeText={(e) => {
-              setSearchText(e);
-            }}
-          ></TextInput></View>
-        <View><TouchableOpacity onPress={() => searchList()} style={styles.btn}>
-          <Text style={styles.btntxt}>Search Now</Text>
-        </TouchableOpacity></View>
-
+      />
+      <View style={styles.headerCtr}>
+        <TextInput style={styles.header}>Sitemate Assigment</TextInput>
       </View>
 
 
-      <View style={styles.flatListCtr}>
+      <View style={styles.bodyCtr}>
+        <View style={styles.inputCtrl}>
+          <View style={{ flex: 1 }}>
+            <TextInput style={styles.input}
+            placeholder='Enter Your Query'
+              value={searchTxt}
+              onChangeText={(e) => {
+                setSearchText(e);
+              }}
+            ></TextInput></View>
+          <View><TouchableOpacity onPress={() => searchList()} style={styles.btn}>
+            <Text style={styles.btntxt}>Search Now</Text>
+          </TouchableOpacity></View>
+
+        </View>
+
+        <View style={styles.flatListCtr}>
 
 
 
 
-        {
-          loader ? <ActivityIndicator ></ActivityIndicator> : <View>
-            <Text>Total Results: {totalRecords}</Text>
+          {
+            loader ? <ActivityIndicator ></ActivityIndicator> : <View>
+              <Text>Total Results: {totalRecords}</Text>
 
-            <FlatList
-              data={results}
-              renderItem={({ item }) => <FlatItem
-                author={item.author}
-                title={item.author}
-                description={item.description}
-                publishedAt={item.publishedAt}
-              />}
-              keyExtractor={item => item.url}
-            />
-          </View>
-        }
+              <FlatList
+                data={results}
+                renderItem={({ item }) => <FlatItem
+                  author={item.author}
+                  title={item.author}
+                  description={item.description}
+                  publishedAt={item.publishedAt}
+                />}
+                keyExtractor={(item, index) => `${index}`}
+              />
+            </View>
+          }
 
 
+
+        </View>
 
       </View>
+
 
 
 
@@ -85,18 +98,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    paddingTop: 100,
-    // alignItems: 'center',
-    // justifyContent: 'center',
+
+
+  },
+  headerCtr: {
+
+    backgroundColor: primaryColor
+  },
+  header:{
+    padding:20,
+
+    fontSize:20,
+    color:'white'
+  },
+
+  bodyCtr: {
+
+    padding: 10,
+    flex:1
+
   },
   flatListCtr: {
-    padding: 10,
     flex: 1,
+    paddingVertical:10
   },
   inputCtrl: {
 
     flexDirection: 'row',
-    padding: 10
+
   },
   input: {
     borderWidth: 1,
@@ -106,7 +135,7 @@ const styles = StyleSheet.create({
   },
   btn: {
     padding: 12,
-    backgroundColor: '#211f4a'
+    backgroundColor: primaryColor
   },
   btntxt: {
     color: 'white'
--- request.js
+++ request.js
@@ -2,6 +2,10 @@ import Request from "./axios"
 
 export function sendOtpAction({searchTxt}) {
     return new Promise( async (next) => {
+       
+        if(!searchTxt){
+            searchTxt=null
+        }
         console.log("searchTxt",searchTxt)
         try {
             let {data: resp} = await Request.get(`/v2/everything?q=${searchTxt}&from=2024-05-18&sortBy=publishedAt&apiKey=2a23b0b46ea94f839aeef592bf82cb7b`)
